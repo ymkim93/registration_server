e3ec2a892472165f17cf4e33556184375fac1e4c
ymkim <ymkim@gluesys.com>
2018-07-18 19:58:19 +0900

    registration api server

--- server.py
+++ server.py
@@ -1,1 +1,11 @@
 from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/create', method=['POST'])
+def test():
+	dict_d = json.loads(request.json)
+
+if __name__=='__main__':
+	app.run(host='0.0.0.0')
